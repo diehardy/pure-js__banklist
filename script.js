03c7e6234ec85b75ef1c1289f78b94496dfb0239
Nikita <nerdyaus21@gmail.com>
2024-06-11 18:02:52 +0300

    feat: add dates + fixed

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
 };
 
 const account2 = {
@@ -13,19 +23,49 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
 };
 
 const account3 = {
   owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
   pin: 3333,
 };
 
 const account4 = {
   owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
+  movements: [430, 1000, 700, 50, 90, 500, +1000, 1760],
   interestRate: 1,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
   pin: 4444,
 };
 
@@ -59,18 +99,19 @@ let inputClosePin = document.querySelector('.form__input--pin');
 
 let isSorted = false
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = ''
   if (sort) {
     isSorted = true
-    movements = [...movements].sort((a, b) => a - b)
+    acc.movements = [...acc.movements].sort((a, b) => a - b)
   } else isSorted = false
-  const movementsToInsert = movements.map((mov, i) => {
+  const movementsToInsert = acc.movements.map((mov, i) => {
     return `        
         <div class="movements__row">
           <div class="movements__type movements__type--${mov > 0 ? 'deposit' : 'withdrawal'}">
           ${i + 1} ${mov > 0 ? 'deposit' : 'withdrawal'}
           </div>
+          <div class="movements__date">${new Date(acc.movementsDates[i]).toLocaleDateString()}</div>
           <div class="movements__value">${mov}$</div>
         </div>
       `;
@@ -152,7 +193,7 @@ const findAccount = function (login, pin) {
 
 
 
-    updateUI()
+    updateUI(authorizedAccount)
 
   }
   else console.log(`nope, you didn't make it!`);
@@ -174,7 +215,9 @@ btnTransfer.addEventListener('click', function (e) {
   let accountTo = accounts.find(acc => acc.username === inputTransferTo.value)
   if (accountTo && authorizedAccount.balance >= transferMoney && transferMoney > 0 && accountTo !== authorizedAccount) {
     authorizedAccount.movements.push(-transferMoney)
+    authorizedAccount.movementsDates.push(new Date().toISOString())
     accountTo.movements.push(transferMoney)
+    accountTo.movementsDates.push(new Date().toISOString())
     transferMoney = 0
     accountTo = '';
     inputTransferAmount.value = '';
@@ -184,16 +227,16 @@ btnTransfer.addEventListener('click', function (e) {
   else {
     alert('something went wrong!')
   }
-  updateUI()
+  updateUI(authorizedAccount)
 
 })
 
 
 
-const updateUI = function () {
-  calcPrintSummary(authorizedAccount)
-  calcPrintBalance(authorizedAccount)
-  displayMovements(authorizedAccount.movements)
+const updateUI = function (acc) {
+  calcPrintSummary(acc)
+  calcPrintBalance(acc)
+  displayMovements(acc)
 }
 
 
@@ -223,8 +266,10 @@ btnLoan.addEventListener('click', function (e) {
 
   if (amount > 0 && authorizedAccount.movements.some(mov => mov >= amount * 0.1)) {
     authorizedAccount.movements.push(amount)
+    authorizedAccount.movementsDates.push(new Date().toISOString())
+    inputLoanAmount.value = ''
   }
-  updateUI()
+  updateUI(authorizedAccount)
 })
 
 
@@ -232,12 +277,9 @@ let flag = 0
 
 btnSort.addEventListener('click', function (e) {
   e.preventDefault()
-
-
-  displayMovements(authorizedAccount.movements, !isSorted)
-
-
-
-
+  displayMovements(authorizedAccount, !isSorted)
 })
+
+
+labelDate.textContent = new Date().toLocaleDateString()
 
